0f95b1360d44219472c132b89d100963682ab20c
Yumin Cho <dbals081216@gmail.com>
2024-01-24 16:20:01 +0900

    :sparkles: update publications page

--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -1,18 +1,64 @@
-import { PUBLICATIONS } from '@/data/publications'
+'use client'
+
+import React from 'react'
+import styled from '@emotion/styled'
+
+import { PUBLICATIONS, YearTypes } from '@/data/publications'
+import PublicationCard from '@/components/PublicationCard'
+
+const Main = styled.main`
+  padding: 40px 96px;
+`
+
+const PageTitle = styled.h1`
+  margin: 0;
+  font-size: ${props => props.theme.fontSize.title_lg};
+`
+
+const Sections = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`
+
+const SectionTitle = styled.h2`
+  margin: 0;
+  font-size: ${props => props.theme.fontSize.title_md};
+  font-weight: ${props => props.theme.fontWeight.title_md};
+  color: ${props => props.theme.color.orange900};
+`
+
+const SectionContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`
 
 export default function Page() {
   return (
-    <main>
-      {PUBLICATIONS.map(({ title, authors }, i) => (
-        <div key={i}>
-          <div>{title}</div>
-          {authors.map(({ name, lastName }, i) => (
-            <span key={i}>
-              {name} {lastName}
-            </span>
-          ))}
-        </div>
-      ))}
-    </main>
+    <Main>
+      <Sections>
+        <PageTitle>Publications</PageTitle>
+        {YearTypes.map(
+          (year, i) =>
+            PUBLICATIONS.filter(({ year: y }) => y === year).length >= 1 && (
+              <Section key={i}>
+                <SectionTitle>{year}</SectionTitle>
+                <SectionContent>
+                  {PUBLICATIONS.filter(({ year: y }) => y === year).map(pub => (
+                    <PublicationCard key={i} pub={pub} />
+                  ))}
+                </SectionContent>
+              </Section>
+            )
+        )}
+      </Sections>
+    </Main>
   )
 }
