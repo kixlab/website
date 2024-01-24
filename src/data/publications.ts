0020a569e718af7e6917afd9faeb9e8c3677c3d9
Yumin Cho <dbals081216@gmail.com>
2024-01-24 16:12:47 +0900

    :label: update Publication structure

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -3,6 +3,9 @@ import Member, { MEMBER } from './members'
 export const PublicationTypes = ['conference', 'poster', 'workshop', 'journal', 'preprint'] as const
 export type PublicationType = (typeof PublicationTypes)[number]
 
+export const YearTypes = ['Preprints', 2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017] as const // 0 means preprint
+export type YearType = (typeof YearTypes)[number]
+
 export const ResearchTopic = [
   'datamining',
   'crowdsourcing',
@@ -14,36 +17,73 @@ export const ResearchTopic = [
 ] as const
 export type ResearchTopic = (typeof ResearchTopic)[number]
 
-interface Props {
+export enum PublicationLinkType {
+  ACM = 'ACM DL',
+  WEB = 'Website',
+  VID = 'Video',
+  ARX = 'arXiv',
+}
+
+export interface Publication {
   title: string
   authors: Member[]
-  year: number
+  year: YearType
   venue: string
   topics: ResearchTopic[]
   type: PublicationType
-  award: string
-  link: string
+  award?: string
+  links?: { url: string; type: PublicationLinkType }[]
 }
 
-interface Publication extends Props {}
-class Publication {
-  constructor(attrs: Props) {
-    Object.assign(this, attrs)
-  }
-}
-
-export const PUBLICATION = {
-  codetree: new Publication({
+export const PUBLICATIONS: Publication[] = [
+  {
     title: 'CodeTree: A System for Learnersourcing Subgoal Hierarchies in Code Examples',
     authors: [MEMBER.hyoungwookjin, MEMBER.juhokim],
     venue: 'CSCW 2024',
     year: 2024,
     topics: ['crowdsourcing', 'learning'],
     type: 'conference',
+  },
+  {
+    title: 'example1: name of paper',
+    authors: [MEMBER.example1],
+    venue: 'CHI 2018',
+    year: 2018,
+    topics: ['civics', 'visualization'],
+    type: 'conference',
+    links: [
+      { url: 'https://dl.acm.org', type: PublicationLinkType.ACM },
+      { url: 'https://kixlab.org', type: PublicationLinkType.WEB },
+      { url: 'https://youtube.com', type: PublicationLinkType.VID },
+      { url: 'https://arxiv.org', type: PublicationLinkType.ARX },
+    ],
+  },
+  {
+    title: 'example: this is preprint',
+    authors: [MEMBER.example1],
+    venue: 'CHI 2018',
+    year: 'Preprints',
+    topics: ['civics', 'visualization'],
+    type: 'conference',
+    award: 'best example award',
+    links: [],
+  },
+  {
+    title: 'example1: name of paper',
+    authors: [MEMBER.example1],
+    venue: 'CHI 2018',
+    year: 2018,
+    topics: ['civics', 'visualization'],
+    type: 'conference',
     award: '',
-    link: '',
-  }),
-} as const
-
-export const PUBLICATIONS = Object.values(PUBLICATION).sort((a, b) => a.year - b.year)
-export default PUBLICATIONS
+  },
+  {
+    title: 'example: this is preprint',
+    authors: [MEMBER.example1],
+    venue: 'CHI 2018',
+    year: 'Preprints',
+    topics: ['civics', 'visualization'],
+    type: 'conference',
+    award: 'best example award',
+  },
+] as const
