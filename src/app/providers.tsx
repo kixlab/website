36f16c900aad06e13812411e2983c67ed25e9165
Yumin Cho <dbals081216@gmail.com>
2024-01-26 18:49:09 +0900

    :lipstick: add global focus-visible style

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,23 @@
 
 import { ThemeProvider } from '@emotion/react'
 import { theme } from '@/app/theme'
+import { Global, css } from '@emotion/react'
 
 interface Props {
   children: React.ReactNode
 }
 
 export const Providers: React.FC<Props> = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          *:focus-visible {
+            outline: 2px solid ${theme.color.orange700};
+          }
+        `}
+      />
+      {children}
+    </ThemeProvider>
+  )
 }
