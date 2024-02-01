8d581b55915ad5581e16c57b2b23c1013df44f97
Yumin Cho <dbals081216@gmail.com>
2024-02-01 18:41:48 +0900

    :label: update Member structure

--- src/data/members.ts
+++ src/data/members.ts
@@ -1,37 +1,115 @@
-interface Props {
-  name: string
+export const CareerTypes = [
+  'Associate Professor',
+  'Postdoc Researcher',
+  'Ph.D. Student',
+  'M.S. Student',
+  'Visiting Researcher',
+  'Undergrad Intern',
+  'Staff',
+  'Alumni',
+] as const
+
+export type CareerType = (typeof CareerTypes)[number]
+
+export interface Member {
+  firstName: string
   lastName: string
   email: string
-  career: string
+  career: CareerType
   img: string
-  site: string
-}
-
-interface Member extends Props {}
-class Member {
-  constructor(attrs: Props) {
-    Object.assign(this, attrs)
-  }
+  site?: string
 }
 
 export const MEMBER = {
-  juhokim: new Member({
-    name: 'Juho',
+  juhokim: {
+    firstName: 'Juho',
     lastName: 'Kim',
     email: 'juhokim@kaist.ac.kr',
     career: 'Associate Professor',
     img: 'juhokim.jpg',
     site: 'http://juhokim.com/',
-  }),
-  hyoungwookjin: new Member({
-    name: 'Hyoungwook',
+  },
+  hyoungwookjin: {
+    firstName: 'Hyoungwook',
     lastName: 'Jin',
     email: 'jinhw@kaist.ac.kr',
     career: 'M.S. Student',
     img: 'hyoungwookjin.jpg',
     site: 'https://jhw123.github.io',
-  }),
-} as const
+  },
+  yumincho: {
+    firstName: 'Yumin',
+    lastName: 'Cho',
+    email: 'dbals1184@kaist.ac.kr',
+    career: 'Undergrad Intern',
+    img: 'yumincho.png',
+  },
+  example1: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@kaist.ac.kr',
+    career: 'M.S. Student',
+    img: 'example.png',
+  },
 
-export const MEMBERS = Object.values(MEMBER)
+  example2: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@kaist.ac.kr',
+    career: 'M.S. Student',
+    img: 'example.png',
+  },
+  example3: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@kaist.ac.kr',
+    career: 'M.S. Student',
+    img: 'example.png',
+  },
+  example4: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@kaist.ac.kr',
+    career: 'M.S. Student',
+    img: 'example.png',
+  },
+  example5: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@kaist.ac.kr',
+    career: 'Ph.D. Student',
+    img: 'example.png',
+  },
+  example6: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@kaist.ac.kr',
+    career: 'Ph.D. Student',
+    img: 'example.png',
+  },
+  example7: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@kaist.ac.kr',
+    career: 'M.S. Student',
+    img: 'example.png',
+  },
+  example8: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@kaist.ac.kr',
+    career: 'M.S. Student',
+    img: 'example.png',
+  },
+  example9: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@kaist.ac.kr',
+    career: 'M.S. Student',
+    img: 'example.png',
+  },
+} as const satisfies Record<string, Member>
+
+export const MEMBERS_KEY = Object.keys(MEMBER)
+export const MEMBERS = MEMBER as Record<string, Member>
 export default Member
