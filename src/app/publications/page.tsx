4abcfc2cd991c3e4380b1862a54f2903fb87e9f5
Yumin Cho <dbals081216@gmail.com>
2024-02-16 20:53:30 +0900

    :sparkles: implement filtering functionality in publications page

--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -1,19 +1,63 @@
 'use client'
 
-// import React from 'react'
+import { useState, useEffect } from 'react'
+import styled from '@emotion/styled'
 
-import { PUBLICATIONS, YearTypes } from '@/data/publications'
+import { PUBLICATIONS, YearTypes, PublicationTypes, ResearchTopics } from '@/data/publications'
+import type { PublicationType, ResearchTopic } from '@/data/publications'
 import PublicationCard from '@/components/PublicationCard'
 import { Sections, Section, SectionTitle, SectionContent } from '@/components/Section'
+import Filter from '@/components/Filter'
+
+const Filters = styled.div`
+  display: flex;
+  gap: 12px;
+`
 
 export default function Page() {
+  const [researchTopic, setResearchTopic] = useState<ResearchTopic | 'All'>('All')
+  const [publicationType, setPublicationType] = useState<PublicationType | 'All'>('All')
+  const [publicationList, setPublicationList] = useState(PUBLICATIONS)
+
+  const handleResearchTopicChange = (topic: string) => {
+    setResearchTopic(topic as ResearchTopic)
+  }
+
+  const handlePublicationTypeChange = (type: string) => {
+    setPublicationType(type as PublicationType)
+  }
+
+  useEffect(() => {
+    setPublicationList(
+      PUBLICATIONS.filter(
+        pub =>
+          (researchTopic === 'All' || pub.topics.includes(researchTopic)) &&
+          (publicationType === 'All' || pub.type === publicationType)
+      )
+    )
+  }, [researchTopic, publicationType])
+
   return (
     <main>
       <h1>Publications</h1>
+      <Filters>
+        <Filter
+          filterName="Research Topic"
+          optionSet={['All', ...ResearchTopics]}
+          optionSelected={researchTopic}
+          handleOptionChange={handleResearchTopicChange}
+        />
+        <Filter
+          filterName="Type"
+          optionSet={['All', ...PublicationTypes]}
+          optionSelected={publicationType}
+          handleOptionChange={handlePublicationTypeChange}
+        />
+      </Filters>
       <Sections>
         {YearTypes.map(
           (year, i) =>
-            PUBLICATIONS.filter(({ year: y }) => y === year).length >= 1 && (
+            publicationList.filter(({ year: y }) => y === year).length >= 1 && (
               <Section key={i}>
                 <SectionTitle>{year}</SectionTitle>
                 <SectionContent>
