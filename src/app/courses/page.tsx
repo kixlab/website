edf70caad4ec0204172975ebbad4b30ac8bf67bd
chanyoung <80759746+itnoj15@users.noreply.github.com>
2024-03-08 15:25:21 +0900

    Add course data and Modify page code (#12)
    * Add course data and Modify page code
    
    * Resolve type error
    
    * Resolve style check
    
    * Resolve minor issues
    
    * Restore original files
    
    * Update import method for 'lodash'
    
    * Delete package-lock.json

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,11 +1,94 @@
+'use client'
+
+import styled from '@emotion/styled'
+import React from 'react'
 import { COURSES } from '@/data/courses'
+import { FontVariant, Color } from '@/app/theme'
+import { chunk } from 'lodash'
 
 export default function Page() {
   return (
     <main>
-      {COURSES.map(({ title }, i) => (
-        <div key={i}>{title}</div>
-      ))}
+      <h1>Courses</h1>
+      <Sections>
+        {COURSES.map(({ code, title, description, editions }, i) => (
+          <React.Fragment key={i}>
+            <Section key={i}>
+              <SectionTitle>
+                {code} {title}
+              </SectionTitle>
+              <SectionContent>{description}</SectionContent>
+              <SectionGroup>
+                {chunk(editions, 3).map((editionGroup: any, j: any) => (
+                  <SectionEditions key={j}>
+                    {editionGroup.map((edition: any, k: any) => (
+                      <SectionEdition key={k} href={edition.url}>
+                        • {edition.semester}
+                      </SectionEdition>
+                    ))}
+                  </SectionEditions>
+                ))}
+              </SectionGroup>
+            </Section>
+            {i < COURSES.length - 1 && <HorizontalLine />}
+          </React.Fragment>
+        ))}
+      </Sections>
     </main>
   )
 }
+
+const Sections = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  width: 80%;
+`
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`
+
+const SectionTitle = styled.h2`
+  margin: 0;
+  ${FontVariant.title_md}
+`
+
+const SectionContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  ${FontVariant.body_md}
+  color: ${Color.gray700}
+`
+
+const SectionGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
+
+const SectionEditions = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const SectionEdition = styled.a`
+  ${FontVariant.body_md}
+  color: ${Color.orange900};
+  text-decoration: none;
+  flex-basis: 170px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const HorizontalLine = styled.hr`
+  border: 0;
+  height: 1px;
+  background-color: ${Color.gray300};
+  width: 100%;
+`
