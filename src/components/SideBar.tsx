e771d2e5507e67913a679ee1023f9c35e2eeb52e
Alex Tio <alextio@kaist.ac.kr>
2024-06-27 00:32:41 +0900

    🎨 Make SideBar more modular (WIP)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,13 +3,12 @@ import React from 'react'
 import { uniq } from 'lodash'
 import styled from '@emotion/styled'
 import { Color, FontVariant } from '@/app/theme'
+import { NAV_BAR_HEIGHT } from './NavBar'
 
 const SideBarContainer = styled.div`
-  width: 15%;
   position: sticky;
-  padding: 96px;
-  padding-left: 0px;
-  top: 0;
+  // TODO: make this more modular by having top take a prop
+  top: ${NAV_BAR_HEIGHT + 40}px;
   height: 100vh;
 `
 
@@ -43,18 +42,21 @@ const Sidebar = ({ activeSection, handleLinkClick, publicationList }: Props) => 
   return (
     <SideBarContainer>
       <SidebarLink
+        // TODO: Make this more modular by having href take a prop instead of hardcoded value
         href="#preprints"
         className={activeSection === 'preprints' ? 'active' : ''}
         onClick={() => handleLinkClick('preprints')}
       >
         Preprints
       </SidebarLink>
+      {/* TODO: Make this more modular by replacing publicationList with a prop */}
       {uniq(publicationList)
         .sort()
         .reverse()
         .map(year => (
           <SidebarLink
             key={year}
+            // TODO: Replace hardcoded values
             href={`#year-${year}`}
             className={activeSection === `year-${year}` ? 'active' : ''}
             onClick={() => handleLinkClick(`year-${year}`)}
