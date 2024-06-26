97b75b58142222585ce51a2a533b86726be66b51
Alex Tio <alextio@kaist.ac.kr>
2024-06-27 00:55:12 +0900

    💄 Change spacing after pub. section title

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled'
 import { FontVariant, Color, ScreenSize, linearlyScaleSize } from '@/app/theme'
 
 export const Sections = styled.div`
+  // TODO: can we combine Sections, Section, SectionTitle, and SectionContent into one component?
   display: flex;
   flex-direction: column;
   gap: 24px;
@@ -10,11 +11,11 @@ export const Sections = styled.div`
 export const Section = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 24px;
 `
 
 export const SectionTitle = styled.h2`
   margin: 0;
+  margin-bottom: 12px;
   ${FontVariant.title_md}
   color: ${Color.orange900};
 `
