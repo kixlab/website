2474f131b65b060081de17dd763052c9d962c9c0
Alex Tio <alextio@kaist.ac.kr>
2024-07-03 14:05:49 +0900

    ⚡️ Use YoutubeEmbed instead of iframe

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -1,12 +1,13 @@
-interface Props {
+interface VideoProps {
   url: string
   title: string
+  id: string
   date: Date
 }
 
-interface Video extends Props {}
+interface Video extends VideoProps {}
 class Video {
-  constructor(attrs: Props) {
+  constructor(attrs: VideoProps) {
     Object.assign(this, attrs)
   }
 
@@ -18,16 +19,19 @@ class Video {
 const VIDEOS = [
   new Video({
     url: 'https://www.youtube.com/embed/j0v1Cr74kN8?si=tp_blkpKqN4FP9te',
+    id: 'j0v1Cr74kN8',
     title: 'What is Interaction-centric AI?',
     date: new Date('2022-10-28'),
   }),
   new Video({
     url: 'https://www.youtube.com/embed/pkhTuiYvvw4?si=hUee7IqJ-m95L1k2',
+    id: 'pkhTuiYvvw4',
     title: 'KIXLAB Introduction',
     date: new Date('2021-02-12'),
   }),
   new Video({
     url: 'https://www.youtube.com/embed/GgvkmXXPFPI?si=YWMLcLMhac5kRYzJ',
+    id: 'GgvkmXXPFPI',
     title: 'Open KAIST',
     date: new Date('2022-01-10'),
   }),
