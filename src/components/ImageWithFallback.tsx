9caa44b9a62c3164cfaf3c4e4e70ab5f4eea133d
Alex Tio <alextio@kaist.ac.kr>
2024-09-05 23:58:14 +0900

    ⚡️ Lazy load People images

--- src/components/ImageWithFallback.tsx
+++ src/components/ImageWithFallback.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import Image, { ImageProps } from 'next/image'
+import { useInView } from 'react-intersection-observer'
 
 export const ImageWithFallback = ({ fallbackSrc, ...props }: { fallbackSrc: string } & ImageProps) => {
   const [imageSrc, setImageSrc] = useState(props.src)
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: '200px 0px',
+  })
 
   const onError = () => {
     setImageSrc(fallbackSrc)
   }
 
-  return <Image {...props} src={imageSrc} alt={props.alt} onError={onError} />
+  return <div ref={ref}>{inView ? <Image {...props} src={imageSrc} alt={props.alt} onError={onError} /> : null}</div>
 }
