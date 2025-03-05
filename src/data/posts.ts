cec5119e7fe1023695f37cb93530d2f3944a6531
Jenny Y <jenny.c.yang@outlook.com>
2025-03-05 18:00:37 +0900

    Added CHI25 papers to news

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -3,7 +3,7 @@ interface Props {
   date: Date
   categories: readonly string[]
   summary: string
-  contentMdFilePath?: string // Add path to markdown file if exists. Path should be relative to /public/posts/
+  contentMdFilePath?: string // Add path to markdown file if exists
   endsAt?: Date // Add if the post has a deadline
 }
 
@@ -16,32 +16,12 @@ export class Post {
 
 export const POSTS: Post[] = [
   {
-    title: 'Three New Students Join KIXLAB',
-    date: new Date('2025-02-19'),
+    title: 'KIXLAB members and papers at CHI 2025',
+    date: new Date('2025-01-17'),
     categories: ['news'],
     summary:
-      'Three new grad students have joined KIXLAB starting from spring 2025. Welcome Jennie (Ph.D), Heechan (M.S.), and Jaesang (M.S.)!',
-  },
-  {
-    title: 'Dae Hyun Becomes Professor at Yonsei University',
-    date: new Date('2025-02-18'),
-    categories: ['news'],
-    summary:
-      'Dae Hyun Kim officially completed his postdoc at KIXLAB and is now an assistant professor at Yonsei University. Congratulations, Dae Hyun!',
-  },
-  {
-    title: 'Three KIXLAB Members Graduate from KAIST',
-    date: new Date('2025-02-14'),
-    categories: ['news'],
-    summary:
-      "Three KIXLAB members graduated from KAIST. Congratulations to Dr. Seoyoung Kim, KIXLAB's second Ph.D graduate(!) and to Jihyeong and Junho, KIXLAB's newly minted M.S. graduates!",
-  },
-  {
-    title: 'Seoyoung Awarded Outstanding Ph.D Thesis Award from KAIST School of Computing!',
-    date: new Date('2025-02-11'),
-    categories: ['award'],
-    summary:
-      'Seoyoung Kim was awarded the prestigious Outstanding Ph.D Thesis Award from the KAIST School of Computing.',
+      'Four papers from KIXLAB have been conditionally accepted to [CHI 2025](https://chi2025.acm.org/).',
+    contentMdFilePath: '2025-01-17-chi2025.md',
   },
   {
     title: 'KIXLAB members and papers at CSCW 2024',
