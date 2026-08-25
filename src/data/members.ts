9d434896c0538ceedaa64e2605068cf9559a76a2
deanvilgel <deanvilgel21@gmail.com>
2026-08-25 19:35:40 +0900

    fix: update Member interface syntax for consistency

--- src/data/members.ts
+++ src/data/members.ts
@@ -42,7 +42,7 @@ interface Props {
   currentPosition?: string // for alumni
 }
 
-export interface Member extends Props { }
+export interface Member extends Props {}
 export class Member {
   constructor(attrs: Props) {
     Object.assign(this, attrs)
